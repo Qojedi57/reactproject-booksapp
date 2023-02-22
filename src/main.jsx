c711e3a2c0046686f452ae55b6a45d90376860df
Qojedi57 <111308861+Qojedi57@users.noreply.github.com>
2023-02-21 18:48:00 -0800

    changed the file name

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import AppContextProvider from "./components/context/app-context";
 import { ChakraProvider } from "@chakra-ui/react";
+import App from "./App";
 
  ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
